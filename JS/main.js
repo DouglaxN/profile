e4d33bfd22d9d71d1959ddc2df7afb66f73df675
Douglas Nunes <douglas.devx@gmail.com>
2023-12-26 12:40:38 -0300

    projeto faltando só selecinar os textos para cada espaço e tirar as partes que não funcionaram

--- JS/main.js
+++ JS/main.js
@@ -82,3 +82,79 @@ let swiper = new Swiper(".portifolio_container", {
         el: ".swiper-pagination",
     },
 })
+
+// SCROLL SECTIONS ACTIVE LINK
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight // Correção: sectionHeight
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id') // Correção: const sectionId
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+// MUDAR O BACKGROUD DO HEADER
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    if (this.scrollY >= 80) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// MOSTRAR O BOTÃO DE SCROLL UP
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    // Correção: Usar uma expressão condicional para verificar se o scrollY é maior ou igual a 560
+    if (window.scrollY >= 560) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+
+// Correção: Adicionar parênteses para chamar a função
+window.addEventListener('scroll', scrollUp);
+
+// ADICONAR O TEMA ESCURO PARA O SITE
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
